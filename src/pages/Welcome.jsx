dd13be42692cc925bfff5a6886a393e6f5886353
shakhawat.dev <shakhawat.code@gmail.com>
2025-07-21 10:49:24 +0600

    responsive done for weather page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router'
 
 export default function Welcome() {
   return (
-    <div className=' w-full h-screen p-10 bg-[#0b131e] grid grid-cols-2 place-items-center overflow-hidden   '>
+    <div className=' w-full h-screen md:p-10 p-5 bg-[#0b131e] grid md:grid-cols-2 place-items-center overflow-hidden   '>
       
-      <img src="/wether.webp" alt="" className=' w-full h-full object-cover overflow-hidden rounded-[10px]  ' />
+      <img src="/wether.webp" alt="" className=' w-full h-full object-cover overflow-hidden rounded-[10px] hidden md:inline-block  ' />
 
       {/* welcome  */}
       <div className="flex items-center justify-center  flex-col gap-5 text-white font-ubuntu ">
         <img src="/umbrella.png" alt="umbrella" className=" w-[80px]  object-cover overflow-hidden rounded-[10px] " />
 
-        <h2 className="font-bold text-3xl">Welcome to Weather App</h2>
+        <h2 className="font-bold md:text-3xl text-xl ">Welcome to Weather App</h2>
 
         <h3 className="flex items-center gap-2   ">
           <p>Created by -</p>
